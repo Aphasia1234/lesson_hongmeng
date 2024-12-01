cc5abcc984c1a2d407bc92cf817d044d96a6f251
Aphasia1234 <3098350970@qq.com>
2024-12-01 11:38:24 +0800

    修改

--- js/yayu/6.js
+++ js/yayu/6.js
@@ -2,16 +2,16 @@
       this.name= name;
       this.age= age;
  }
- function Person(){
-    
- }
+//  function Person(){
+
+//  }
  Person.prototype.name= '孔子'
  Person.prototype.age= 18
  Person.prototype.hometown= '山东'
- let person1= new Person();
- let person2= new Person();
-//  let person1= new Person('张三',20);
-//  let person2= new Person('李四',21);
+//  let person1= new Person();
+//  let person2= new Person();
+  let person1= new Person('张三',20);
+  let person2= new Person('李四',21);
  console.log(person1===person2);//名字相同 但是不是同一个对象
  console.log(person1.name,person2.name);
  console.log(person1.age,person2.age);
