0ead89bb3556f516201aeff13c1ceb8d10ad5ec5
Aphasia1234 <3098350970@qq.com>
2024-12-27 17:27:44 +0800

    vue

---
+++ vue_react/cli/try-vue/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<!-- 一个典型的 App.vue 文件由三个主要部分组成：
+     <template>、<script> 和 <style> -->
+
+<template>
+    <div class="counter">
+        <p>Count: {{ count }}</p>
+        <button @click="increment">+1</button>
+    </div>
+</template>
+
+<script>
+import {
+    ref
+} from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+    count.value++
+}
+</script>
+
+<style>
+.counter {
+    text-align: center;
+    margin-top: 50px;
+}
+button {
+    padding :10px 20px;
+    font-size: 16px;
+}
+</style>
