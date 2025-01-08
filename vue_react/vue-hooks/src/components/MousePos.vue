469451b28479bc4f7a6fbbc7863e7dfc39f9624b
Aphasia1234 <3098350970@qq.com>
2025-01-08 19:57:37 +0800

    正则

---
+++ vue_react/vue-hooks/src/components/MousePos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>Mouse X: {{ x }}</p>
+        <p>Mouse Y: {{ y }}</p>
+    </div>
+</template>
+
+<script setup>
+// 数据剥离 响应式与UI分离 使组件focus于页面
+import {useMouse,useMemo} from '../hooks/useMouse'
+import Hook from '../hooks/useMouse'
+const {x,y} = useMouse()
+useMemo()
+
+console.log(Hook)
+</script>
+
+<style scoped>
+
+</style>
