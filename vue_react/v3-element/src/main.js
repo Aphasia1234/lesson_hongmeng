8b758a257cc2ce1454a02f8cce38685e83d9ca68
Aphasia1234 <3098350970@qq.com>
2025-01-28 00:09:00 +0800

    引入pinia，安装stylus，引入全局css

--- vue_react/v3-element/src/main.js
+++ vue_react/v3-element/src/main.js
@@ -6,9 +6,11 @@ import {
 } from 'element3'
 import 'element3/lib/theme-chalk/index.css'// 引入样式
 import router from './router'
+import {createPinia} from 'pinia'
 
 
 createApp(App)
   . use(router)
+  .use(createPinia())
   .use(ElButton)  // UI组件库 开发效率提升了
   .mount('#app')
